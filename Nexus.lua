343a9b05166894d3c80658ffa8edec5244474418
v3rt3x <74108066+yaboi-v3rt3x@users.noreply.github.com>
2024-07-14 02:08:29 -0400

    Update Nexus.lua

--- Nexus.lua
+++ Nexus.lua
@@ -35,7 +35,7 @@ local SubHubs = Window:CreateTab("Sub Hubs", 4483362458)
 local Button = Tab:CreateButton({
    Name = "Button Example",
    Callback = function()
-      loadstring(game:HttpGet("https://raw.githubusercontent.com/yaboi-v3rt3x/Nexus/main/Sub-Hubs/Prison%20Life.lua"))()
+      game:HttpGet("https://raw.githubusercontent.com/yaboi-v3rt3x/Nexus/main/Sub-Hubs/Prison%20Life.lua")
    end,
 })
 
